57e4f07e2a1918b88f7ceaf304d44bf9ca967da6
Marc Mintel <marc.mintel@i22.de>
2017-08-18 14:59:07 +0200

    update

--- src/components/headline.js
+++ src/components/headline.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-fela';
+import ms from 'modularscale-js';
 
 const Headline = ({
   tag,
