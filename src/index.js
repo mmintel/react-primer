0b7882adb49c5c68177fb1d5a70118171a8c3c95
Marc Mintel <marc@mintel.me>
2017-09-26 21:11:25 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 export { default as Button } from './components/button';
 export { default as Container } from './components/container';
-export { default as Image } from './components/image';
-export { default as Headline } from './components/headline';
+export { default as Accordion } from './components/accordion';
+export { default as Expand } from './components/expand';
 export { default as Dimmer } from './components/dimmer';
+export { default as Headline } from './components/headline';
+export { default as Image } from './components/image';
+export { default as Loading } from './components/loading';
