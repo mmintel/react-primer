d6ba22d0762a306034c10690eef3e8ff4062caf4
Marc Mintel <marc@mintel.me>
2018-01-26 20:32:58 +0100

    update

--- src/hocs/with-margins.jsx
+++ src/hocs/with-margins.jsx
@@ -3,21 +3,29 @@ import { connect } from 'react-fela';
 import cn from 'classnames';
 
 function withMargins(WrappedComponent) {
-  const StyledComponent = ({ margin, styles, className, ...props }) => {
-    console.log(styles)
-    return <WrappedComponent className={cn(styles.margin, className)} {...props} />
-  }
+  const StyledComponent = ({ m, styles, className, ...props }) =>
+    <WrappedComponent className={cn(styles.margin, className)} {...props} />
 
   StyledComponent.defaultProps = {
     margin: {},
   };
 
-  const margin = props => ({
-    marginTop: props.margin.top && props.theme.calculateSpacing(props.margin.top),
-    marginRight: props.margin.right && props.theme.calculateSpacing(props.margin.right),
-    marginBottom: props.margin.bottom && props.theme.calculateSpacing(props.margin.bottom),
-    marginLeft: props.margin.left && props.theme.calculateSpacing(props.margin.left),
-  });
+  const margin = ({ m, theme }) => {
+    if (!m) return;
+    const { t, r, b, l, ...media } = m;
+    console.log(media)
+    return {
+      marginTop: t && theme.calculateSpacing(t),
+      marginRight: r && theme.calculateSpacing(r),
+      marginBottom: b && theme.calculateSpacing(b),
+      marginLeft: l && theme.calculateSpacing(l),
+      ...(() => (
+        for(item of media) {
+          
+        }
+      )),
+    }
+  };
 
   return connect({
     margin,
